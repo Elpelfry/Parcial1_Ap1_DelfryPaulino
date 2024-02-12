cc6211446452af9935c39691bf388f259648213b
Elpelfry <delfry_paulino@ucne.edu.do>
2024-02-12 19:57:02 -0400

    Create

--- Models/Metas.cs
+++ Models/Metas.cs
@@ -6,9 +6,9 @@ public class Metas
 {
     [Key]
     public int MetaId { get; set; }
-    [Required(ErrorMessage = "La fecha es obligatoria")]
+    [Required(ErrorMessage = "La Fecha es obligatoria")]
     public DateTime Fecha { get; set; }
-    [Required(ErrorMessage = "La Descripcion es obligatoria")]
+    [Required(ErrorMessage = "La Descripción es obligatoria")]
     public string? Descripcion { get; set; }
     [Range(0, double.MaxValue)]
     public double Monto { get; set;}
