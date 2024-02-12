bd893f33feeddeb689953ff22ff9105447b5b358
Elpelfry <delfry_paulino@ucne.edu.do>
2024-02-12 18:42:38 -0400

    Program connection String

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
+using Microsoft.EntityFrameworkCore;
 using Parcial1_Ap1_DelfryPaulino.Components;
+using Parcial1_Ap1_DelfryPaulino.DAL;
+using Parcial1_Ap1_DelfryPaulino.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
+builder.Services.AddDbContext<Contexto>(options => options.UseSqlite(ConStr));
+builder.Services.AddScoped<MetasService>();
 
 var app = builder.Build();
 
