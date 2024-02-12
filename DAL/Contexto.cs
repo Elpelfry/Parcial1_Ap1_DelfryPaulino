97086e5e5dd1158beace6cd5bcea9139af57393a
Elpelfry <delfry_paulino@ucne.edu.do>
2024-02-12 18:23:30 -0400

    Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial1_Ap1_DelfryPaulino.Models;
+
+namespace Parcial1_Ap1_DelfryPaulino.DAL;
+
+public class Contexto : DbContext
+{
+    public DbSet<Metas> Metas { get; set; }
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+}
