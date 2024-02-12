d08a7be0d90a7013d26aaed80626dd51f6694926
Elpelfry <delfry_paulino@ucne.edu.do>
2024-02-12 18:19:24 -0400

    Modelo Metas

---
+++ Models/Metas.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial1_Ap1_DelfryPaulino.Models;
+
+public class Metas
+{
+    [Key]
+    public int MetaId { get; set; }
+    [Required(ErrorMessage = "La fecha es obligatoria")]
+    public DateTime Fecha { get; set; }
+    [Required(ErrorMessage = "La Descripcion es obligatoria")]
+    public string? Descripcion { get; set; }
+    [Range(0, double.MaxValue)]
+    public double Monto { get; set;}
+
+}
