49ccb91f253844ef878dd9d4c019a3b1417af0fb
Elpelfry <delfry_paulino@ucne.edu.do>
2024-02-12 20:10:51 -0400

    Validar mensaje

--- Models/Metas.cs
+++ Models/Metas.cs
@@ -10,7 +10,7 @@ public class Metas
     public DateTime Fecha { get; set; }
     [Required(ErrorMessage = "La Descripción es obligatoria")]
     public string? Descripcion { get; set; }
-    [Range(1000, double.MaxValue)]
+    [Range(1000, double.MaxValue, ErrorMessage ="El Monto debe ser mayor a 1000")]
     public double Monto { get; set;}
 
 }
