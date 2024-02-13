9720a20e90e073b667765fa91973268c6f9ff21d
Elpelfry <delfry_paulino@ucne.edu.do>
2024-02-12 20:01:16 -0400

    condicion a monto

--- Models/Metas.cs
+++ Models/Metas.cs
@@ -10,7 +10,7 @@ public class Metas
     public DateTime Fecha { get; set; }
     [Required(ErrorMessage = "La Descripción es obligatoria")]
     public string? Descripcion { get; set; }
-    [Range(0, double.MaxValue)]
+    [Range(1000, double.MaxValue)]
     public double Monto { get; set;}
 
 }
